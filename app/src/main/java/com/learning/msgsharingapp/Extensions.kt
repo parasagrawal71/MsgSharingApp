bd7700dc8c308648c077c86001b0a06db8baa8e8
Paras Agrawal <paras@piktorlabs.com>
2020-02-28 00:07:40 +0530

    Add Default Parameter

--- app/src/main/java/com/learning/msgsharingapp/Extensions.kt
+++ app/src/main/java/com/learning/msgsharingapp/Extensions.kt
@@ -8,6 +8,6 @@ import android.widget.Toast
 //    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 //}
 
-fun Context.showToast(message: String){
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Context.showToast(message: String, duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(this, message, duration).show()
 }
