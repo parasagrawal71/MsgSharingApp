5a8fc37d831a817448460761c159bb9ac28263c9
Paras Agrawal <paras@piktorlabs.com>
2020-02-27 14:50:28 +0530

    Toast Message on click of a button

--- app/src/main/java/com/learning/msgsharingapp/MainActivity.kt
+++ app/src/main/java/com/learning/msgsharingapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.learning.msgsharingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener{
+            // Code
+            Log.i("MainActivity", "Btn was clicked")
+            Toast.makeText(this, "Show Toast Button was clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
